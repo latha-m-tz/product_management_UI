fa8e2d35468818b1859827cd7bcec0a7cd272e53
Mohanraj <mohanraj.s@tamilzorous.com>
2025-10-08 12:10:44 +0530

    api

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:8000/api";
+export const API_BASE_URL = "http://api-vci.tamilzorous.com/api/";
 // export const API_BASE_URL = "http://192.168.1.61:8000/api";
