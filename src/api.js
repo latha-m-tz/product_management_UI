37d3742a7379b62525775c34cd9f21c1e061b3e7
Mohanraj <mohanraj.s@tamilzorous.com>
2025-12-01 14:45:58 +0530

    app

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 // export const API_BASE_URL = "http://192.168.1.61:8000/api";
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8000/api";
+export const API_BASE_URL = "http://api-vci.tamilzorous.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
